40bf9bbd5ab93405336bd1b644a13268c83598da
ThinhTran <thinhtran3803@gmail.com>
2024-10-24 20:26:38 +0700

    init

---
+++ src/main/java/online/thinhtran/psyconnect/PsyConnectApplication.java
@@ -0,0 +1,13 @@
+package online.thinhtran.psyconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsyConnectApplication.class, args);
+	}
+
+}
