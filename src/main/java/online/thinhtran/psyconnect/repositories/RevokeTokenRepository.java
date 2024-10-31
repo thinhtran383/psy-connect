70685953c9dc88441338d1b5f29820d32ca58405
ThinhTran <thinhtran3803@gmail.com>
2024-10-31 14:04:21 +0700

    rename

--- src/main/java/online/thinhtran/psyconnect/repositories/RevokeTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package online.thinhtran.psyconnect.repositories;
-
-import online.thinhtran.psyconnect.entities.RevokeToken;
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RevokeTokenRepository extends KeyValueRepository<RevokeToken, String> {
-    boolean existsByToken(String token);
-}
