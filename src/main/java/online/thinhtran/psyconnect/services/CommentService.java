90faff524991cb2368c32e3eae2061fb597d4440
ThinhTran <thinhtran3803@gmail.com>
2024-11-01 08:52:23 +0700

    add get all comment

--- src/main/java/online/thinhtran/psyconnect/services/CommentService.java
+++ src/main/java/online/thinhtran/psyconnect/services/CommentService.java
@@ -2,8 +2,14 @@ package online.thinhtran.psyconnect.services;
 
 import lombok.RequiredArgsConstructor;
 import online.thinhtran.psyconnect.repositories.CommentRepository;
+import online.thinhtran.psyconnect.responses.PageableResponse;
+import online.thinhtran.psyconnect.responses.comments.UserCommentResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +17,18 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     @Transactional(readOnly = true)
-    public long countByPostId(Integer postId){
+    public long countByPostId(Integer postId) {
         return commentRepository.countByPost_Id(postId);
+    }
+
+
+    @Transactional(readOnly = true)
+    public PageableResponse<UserCommentResponse> getAllCommentsByPostId(Integer postId, int page, int size) {
+        Page<UserCommentResponse> comments = commentRepository.findAllByPostId(postId, PageRequest.of(page, size));
+        return PageableResponse.<UserCommentResponse>builder()
+                .elements(comments.getContent())
+                .totalElements(comments.getTotalElements())
+                .totalPages(comments.getTotalPages())
+                .build();
     }
 }
