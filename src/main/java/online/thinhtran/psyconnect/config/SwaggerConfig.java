6de0e28df4b26bffd41aaf93e69f1426c0b2fab5
ThinhTran <thinhtran3803@gmail.com>
2024-11-01 08:52:38 +0700

    add config jwt

--- src/main/java/online/thinhtran/psyconnect/config/SwaggerConfig.java
+++ src/main/java/online/thinhtran/psyconnect/config/SwaggerConfig.java
@@ -2,28 +2,33 @@ package online.thinhtran.psyconnect.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
 
         servers = {
                 @Server(url = "http://localhost:8080", description = "Server URL cho phát triển"),
-                @Server(url = "https://api.thinhtran.online", description = "Server URL cho production")
+                @Server(url = "https://local.thinhtran.online", description = "Server URL cho production")
         }
 )
 @Configuration
 public class SwaggerConfig {
-//    private SecurityScheme createAPIKeyScheme() {
-//        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-//                .bearerFormat("JWT")
-//                .scheme("bearer");
-//    }
-//
-//    @Bean
-//    public OpenAPI openAPI() {
-//        return new OpenAPI().addSecurityItem(new SecurityRequirement().
-//                        addList("Bearer Authentication"))
-//                .components(new Components().addSecuritySchemes
-//                        ("Bearer Authentication", createAPIKeyScheme()));
-//    }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()));
+    }
 }
