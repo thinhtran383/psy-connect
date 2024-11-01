94cafe379f2070aa316c847e2e8b5907a5b9a246
ThinhTran <thinhtran3803@gmail.com>
2024-11-01 08:51:35 +0700

    add like post

--- src/main/java/online/thinhtran/psyconnect/services/PostLikeService.java
+++ src/main/java/online/thinhtran/psyconnect/services/PostLikeService.java
@@ -1,6 +1,9 @@
 package online.thinhtran.psyconnect.services;
 
 import lombok.RequiredArgsConstructor;
+import online.thinhtran.psyconnect.entities.Post;
+import online.thinhtran.psyconnect.entities.PostLike;
+import online.thinhtran.psyconnect.entities.User;
 import online.thinhtran.psyconnect.repositories.PostLikeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,5 +16,15 @@ public class PostLikeService {
     @Transactional(readOnly = true)
     public long countByPostId(Integer postId){
         return postLikeRepository.countByPost_Id(postId);
+    }
+
+    @Transactional
+    public void likePost(Integer postId, Integer userId){
+        PostLike postLike = PostLike.builder()
+                .post(Post.builder().id(postId).build())
+                .user(User.builder().id(userId).build())
+                .build();
+
+        postLikeRepository.save(postLike);
     }
 }
