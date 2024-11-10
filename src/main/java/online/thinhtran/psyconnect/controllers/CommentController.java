682d096b4925b01f6a4a5c853e7b0ce330af8557
ThinhTran <thinhtran3803@gmail.com>
2024-11-10 12:49:02 +0700

    endpoint get comment and rate doctor

--- src/main/java/online/thinhtran/psyconnect/controllers/CommentController.java
+++ src/main/java/online/thinhtran/psyconnect/controllers/CommentController.java
@@ -6,7 +6,9 @@ import online.thinhtran.psyconnect.entities.User;
 import online.thinhtran.psyconnect.responses.PageableResponse;
 import online.thinhtran.psyconnect.responses.Response;
 import online.thinhtran.psyconnect.responses.comments.UserCommentResponse;
+import online.thinhtran.psyconnect.responses.rating.UserRating;
 import online.thinhtran.psyconnect.services.CommentService;
+import online.thinhtran.psyconnect.services.UserRatingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+    private final UserRatingService userRatingService;
 
 
     @GetMapping("/details/{postId}")
@@ -27,6 +30,18 @@ public class CommentController {
         return ResponseEntity.ok(Response.<PageableResponse<UserCommentResponse>>builder()
                 .data(commentService.getAllCommentsByPostId(postId, page, size))
                 .message("Comments fetched successfully")
+                .build());
+    }
+
+    @GetMapping("/rating/{doctorId}")
+    public ResponseEntity<Response<PageableResponse<UserRating>>> getRatingAndReviewByDoctorId(
+            @PathVariable Integer doctorId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return ResponseEntity.ok(Response.<PageableResponse<UserRating>>builder()
+                .data(userRatingService.getAllRatingByDoctorId(doctorId, page, size))
+                .message("Ratings fetched successfully")
                 .build());
     }
 
