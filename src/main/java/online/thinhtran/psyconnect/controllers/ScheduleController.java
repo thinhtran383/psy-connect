224be7d799aa090f34d423638f3b1b05d93501aa
ThinhTran <thinhtran3803@gmail.com>
2024-11-26 17:06:56 +0700

    fix bug cannot get

--- src/main/java/online/thinhtran/psyconnect/controllers/ScheduleController.java
+++ src/main/java/online/thinhtran/psyconnect/controllers/ScheduleController.java
@@ -2,6 +2,7 @@ package online.thinhtran.psyconnect.controllers;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import online.thinhtran.psyconnect.common.StatusEnum;
 import online.thinhtran.psyconnect.dto.schedule.ScheduleDto;
 import online.thinhtran.psyconnect.entities.User;
@@ -14,8 +15,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/schedules")
+@RequestMapping("${api.base-path/schedules")
 @RequiredArgsConstructor
+@Slf4j
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
@@ -39,6 +41,8 @@ public class ScheduleController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
+
+        log.info("Getting schedules for patient: {}", user.getId());
 
         return ResponseEntity.ok(
                 Response.<PageableResponse<ScheduleResponse>>builder()
