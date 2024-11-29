07c20ea359aa93f645ac7de24986431ad2b7f57f
ThinhTran <thinhtran3803@gmail.com>
2024-11-29 08:20:58 +0700

    remove unuse import

--- src/main/java/online/thinhtran/psyconnect/PsyConnectApplication.java
+++ src/main/java/online/thinhtran/psyconnect/PsyConnectApplication.java
@@ -3,7 +3,6 @@ package online.thinhtran.psyconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
